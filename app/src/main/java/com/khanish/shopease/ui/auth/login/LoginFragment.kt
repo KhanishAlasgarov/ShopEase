021dc6d312ca4541f6d0ea542daab5ec5058868a
KhanishAlasgarov <khanisha@code.edu.az>
2024-07-07 12:48:31 +0400

    feat: login ui added to the project

---
+++ app/src/main/java/com/khanish/shopease/ui/auth/login/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.khanish.shopease.ui.auth.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khanish.shopease.R
+import com.khanish.shopease.base.BaseFragment
+import com.khanish.shopease.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(
+    FragmentLoginBinding::inflate
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
